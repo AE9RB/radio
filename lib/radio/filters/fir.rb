031e8707214fc1bf3e5663226d864c337b537103
David Turnbull <dturnbull@gmail.com>
2012-02-27 21:42:12 -0800

    optimized mixing decimation

--- lib/radio/filters/fir.rb
+++ lib/radio/filters/fir.rb
@@ -22,17 +22,26 @@ class Radio
         # ensure we don't accidentally use slower ops for 1:1
         @options.delete(:interpolate) if @options[:interpolate] == 1
         @options.delete(:decimate) if @options[:decimate] == 1.0
-        coef = @options[:fir]
         # split array for when we interpolate and decimate in one step
+        coef = @options[:fir] # use a [1] for half
+        mix = @options[:mix] # use a zero for half
         if @options[:interpolate] and @options[:decimate]
-          @interpolation_fir_coef = coef[0]
-          @decimation_fir_coef = coef[1]
+          if coef
+            @interpolation_fir_coef = coef[0]
+            @decimation_fir_coef = coef[1]
+          end
+          if mix
+            @interpolation_mix = mix[0]
+            @decimation_mix = mix[1]
+          end
         elsif @options[:interpolate]
+          @interpolation_mix = mix
           @interpolation_fir_coef = coef
         else # decimation (use for 1:1 too)
+          @decimation_mix = mix
           @decimation_fir_coef = coef
         end
-        # expand interpolation filter to multiple
+        # expand interpolation filter for matrix by padding with 0
         if @interpolation_fir_coef
           @interpolation_size = @options[:interpolate].to_i
           remainder = @interpolation_fir_coef.size % @interpolation_size
@@ -41,9 +50,9 @@ class Radio
             @interpolation_fir_coef += [0]*(@interpolation_size-remainder)
           end
         end
-        # double up the coef for performance trick
-        @interpolation_fir_coef = setup_build_coef @interpolation_fir_coef
-        @decimation_fir_coef = setup_build_coef @decimation_fir_coef
+        # prepare coef with performance tricks
+        @interpolation_fir_coef = setup_build_coef @interpolation_fir_coef, @interpolation_mix
+        @decimation_fir_coef = setup_build_coef @decimation_fir_coef, @decimation_mix
         # interpolation is shaped to avoid 0*0 ops
         if @interpolation_fir_coef
           @interpolation_fir_size = @interpolation_fir_coef.size / 2 / @interpolation_size
@@ -57,55 +66,66 @@ class Radio
         end
         # decimation allows fractions for digital work
         if @decimation_fir_coef
-          # reverse decimation coefs (interpolation is not reversed)
-          @decimation_fir_coef = @decimation_fir_coef[-1..0]
-          @decimation_fir_size = @decimation_fir_coef.size / 2
-          @decimation_buf_f = NArray.sfloat @decimation_fir_size
-          @decimation_buf_c = NArray.scomplex @decimation_fir_size
-          @decimation_fir_pos = 0
           @decimation_size = @options[:decimate]
           if Float === @decimation_size
             @decimation_pos = 0.0
           else
             @decimation_pos = 0
           end
+          @decimation_fir_size = @decimation_fir_coef.size / 2
+          @decimation_buf_f = NArray.sfloat @decimation_fir_size
+          @decimation_buf_c = NArray.scomplex @decimation_fir_size
+          @decimation_fir_pos = 0
+        end
+        if @decimation_mix
+          @decimation_phase, @decimation_inc = 
+            setup_new_mixer @decimation_mix, @decimation_size
+        end
+        if @interpolation_mix
+          @interpolation_phase, @interpolation_inc = 
+            setup_new_mixer @interpolation_mix, @interpolation_size
         end
         super
       end
       
-      def setup_build_coef coef
+      def setup_new_mixer mix, size
+        return [Complex(1.0,1.0)/Complex(1.0,1.0).abs,
+                Math.exp(Complex(0, PI2 * mix * size))]
+      end
+      
+      def setup_build_coef coef, mix
         return nil unless coef
+        coef = coef.to_a.reverse
+        if mix
+          # The signal is pre-mixed into the filter.
+          # We adjust the master phase every time we filter.
+          # mix_phase *= mix_phase_inc # faster than sin+cos
+          # Take out the rounding errors once in a while with:
+          # mix_phase /= mix_phase.abs
+          rate = PI2 * mix
+          i = -1
+          coef.collect! do |coef|
+            i += 1
+            coef * Math.exp(Complex(0,rate*i))
+          end
+        end
         if Complex === coef[0]
           new_coef = NArray.scomplex coef.size * 2
         else
           new_coef = NArray.sfloat coef.size * 2
         end
-        new_coef[0...coef.size] = coef.to_a
-        new_coef[coef.size..-1] = coef.to_a
+        new_coef[0...coef.size] = coef
+        new_coef[coef.size..-1] = coef
         new_coef
       end
 
     end
 
 
-    module SetupMix
-      private
-      # This trick is slightly faster than cos/sin lookups.
-      # @mix_phase *= @mix_phase_inc
-      # Take out the rounding errors once in a while with:
-      # @mix_phase /= @mix_phase.abs
-      def setup data
-        @mix_phase = Complex(1.0,1.0)/Complex(1.0,1.0).abs
-        @mix_phase_inc = Math.exp(Complex(0, PI2 * @options[:mix]))
-        super
-      end
-    end
-    
     module ComplexMixDecimateFir
       include SetupFir
-      include SetupMix
       def call data
-        @mix_phase /= @mix_phase.abs
+        @decimation_phase /= @decimation_phase.abs
         out_size = data.size / @decimation_size
         out_size += 1 if @decimation_pos < (data.size % @decimation_size)
         out = NArray.scomplex out_size
@@ -117,10 +137,7 @@ class Radio
           space = @decimation_fir_size - @decimation_fir_pos
           actual = [want,remaining,space].min
           new_fir_pos = @decimation_fir_pos + actual
-          @decimation_buf_c[@decimation_fir_pos...new_fir_pos] = 
-          data[i...i+actual].collect do |v|
-            v * @mix_phase *= @mix_phase_inc
-          end
+          @decimation_buf_c[@decimation_fir_pos...new_fir_pos] = data[i...i+actual]
           @decimation_fir_pos = new_fir_pos
           @decimation_fir_pos = 0 if @decimation_fir_pos == @decimation_fir_size
           @decimation_pos += actual
@@ -128,7 +145,8 @@ class Radio
             @decimation_pos -= @decimation_size
             f_start = @decimation_fir_size-@decimation_fir_pos
             f_end = -1-@decimation_fir_pos
-            out[out_count] = @decimation_buf_c.mul_accum @decimation_fir_coef[f_start..f_end], 0
+            j = @decimation_buf_c.mul_accum @decimation_fir_coef[f_start..f_end], 0
+            out[out_count] = j * @decimation_phase *= @decimation_inc
             out_count += 1
           end
           i += actual
@@ -140,7 +158,6 @@ class Radio
     #TODO upgrade with latest tricks
     module FloatMixDecimateFir
       include SetupFir
-      include SetupMix
       def call data
         @mix_phase /= @mix_phase.abs
         out_size = data.size / @decimation_size
@@ -187,9 +204,9 @@ class Radio
         out = NArray.float @interpolation_size, data.size
         index = 0
         data.each do |value|
-          @interpolation_fir_pos = @interpolation_fir_size if @interpolation_fir_pos == 0
-          @interpolation_fir_pos -= 1
           @interpolation_buf_f[@interpolation_fir_pos] = value
+          @interpolation_fir_pos += 1
+          @interpolation_fir_pos = 0 if @interpolation_fir_pos == @interpolation_fir_size
           f_start = @interpolation_fir_size-@interpolation_fir_pos
           f_end = -1-@interpolation_fir_pos
           iq = @interpolation_fir_coef[f_start..f_end, true].mul_accum @interpolation_buf_f, 0
